bc4a467eb31738fbe456ae51bc3108bd92d01fb1
gytjd <hys339631@gmail.com>
2024-04-20 15:05:48 +0900

    [FIX] : HomeController & INDEX 수정

--- src/main/java/GLANDCL/GLANDCL/Controller/Home/HomeController.java
+++ src/main/java/GLANDCL/GLANDCL/Controller/Home/HomeController.java
@@ -2,7 +2,9 @@ package GLANDCL.GLANDCL.Controller.Home;
 
 import GLANDCL.GLANDCL.Annotation.Login.Login;
 import GLANDCL.GLANDCL.Config.Oauth.SessionUser;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final HttpSession httpSession;
 
+    // 메인 화면 - 게시판 목록
     @GetMapping("/")
-    public String home(@Login SessionUser user, Model model) {
-        if(user!=null) {
-            model.addAttribute("userName",user.getName());
+    public String postList(Pageable pageable, Model model) {
+//        Page<Post> posts = postService.findAllPosts(pageable);
+//        model.addAttribute("posts", posts);
+
+        // 세션에서 사용자 정보 꺼내기
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
         }
         return "index";
     }
